e44a1da17464c95469c612df8f77c9e5874a7889
diogoferrari <diogoferrari@gmail.com>
2020-10-26 23:25:39 -0700

    Included data documentation

---
+++ R/hdpGLM_data.R
@@ -0,0 +1,31 @@
+#' Fake data set with 2000 observations
+#'
+#' A dataset containing simulated data about public opinion in different
+#' countries
+#'
+#' @format A data frame with 2000 rows and 6 variables:
+#' \describe{
+#'   \item{support}{support for welfare policies}
+#'   \item{inequality}{levels of inequality in the neighborhood}
+#'   \item{income}{individual-level income}
+#'   \item{ideology}{individual-level ideology}
+#'   \item{country}{country label or index}
+#'   \item{gap}{country-level gender gap in country's provision of public good}
+#' }
+#' @source Simulated data
+"welfare2"
+
+
+#' Fake data set with 2000 observations
+#'
+#' A dataset containing simulated data about public opinion
+#'
+#' @format A data frame with 2000 rows and 4 variables:
+#' \describe{
+#'   \item{support}{support for welfare policies}
+#'   \item{inequality}{levels of inequality in the neighborhood}
+#'   \item{income}{individual-level income}
+#'   \item{ideology}{individual-level ideology}
+#' }
+#' @source Simulated data
+"welfare"
